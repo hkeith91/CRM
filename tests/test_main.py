1ac3bcedb3005fbe5a088e37718c7d76897a6ea8
Hector <hector.keith91@gmail.com>
2025-09-07 18:18:39 -0400

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,10 @@
 from fastapi.testclient import TestClient
-from app.main import app
+from starlette.types import Message
+from app.main import app, hello_world
 
 client = TestClient(app)
 
-
+"""
 def test_create_client_returns_success():
     response = client.post(
         "/api/contacts/", json={"name": "John Doe", "email": "john.doe@example.com"}
@@ -11,3 +12,11 @@ def test_create_client_returns_success():
 
     assert response.status_code == 201
     assert response.json()["name"] == "John Doe"
+"""
+
+
+def test_hello_world():
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World!"}
