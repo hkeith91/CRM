3b9016aac6fe0f61a4938b09033e3b706d22b5b4
Hector <hector.keith91@gmail.com>
2025-09-07 17:50:34 -0400

    Add comment to test CI pipeline

--- app/main.py
+++ app/main.py
@@ -6,8 +6,18 @@ import uuid
 app = FastAPI()
 
 
+# This is a  comment to test the CI pipeline
 class Contact(BaseModel):
     name: str
     email: str
     phone: Optional[str] = None
     company: Optional[str] = None
+
+
+@app.get("/")
+async def hello_world():
+    """
+    This function handles GET requests to the root URL.
+    Returns a simple JSON response
+    """
+    return {"message": "Hello World!"}
