f83182ce9585811e77c59318fa72a04edd943dc3
Hector <hector.keith91@gmail.com>
2025-09-09 10:23:58 -0400

    Add create_contact()

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, status, HTTPException
 from .models.contact import Contact
 from typing import List
 import uuid
@@ -8,6 +8,12 @@ app = FastAPI()
 
 # A temporary db for testing
 contact_db: List[Contact] = []
+
+
+@app.post("/contacts", status_code=status.HTTP_201_CREATED)
+async def create_contact(new_contact: Contact):
+    contact_db.append(new_contact)
+    return {"data": new_contact}
 
 
 @app.get("/")
