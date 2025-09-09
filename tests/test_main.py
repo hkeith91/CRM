c203dadc39366ddbb3412bee23ce3d28633e1500
Hector <hector.keith91@gmail.com>
2025-09-09 09:07:33 -0400

    Update test_create_contact_returns_success()

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,21 @@
 from fastapi.testclient import TestClient
-from starlette.types import Message
-from app.main import app, hello_world
+from app.main import app
 
 client = TestClient(app)
 
-"""
+
 def test_create_client_returns_success():
     response = client.post(
-        "/api/contacts/", json={"name": "John Doe", "email": "john.doe@example.com"}
+        "/api/contacts/",
+        json={
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "john.doe@example.com",
+        },
     )
 
     assert response.status_code == 201
     assert response.json()["name"] == "John Doe"
-"""
 
 
 def test_hello_world():
