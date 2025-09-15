b1cbe613cf8e5fbb39a54ac9a7eb91c8d7b3a381
Hector <hector.keith91@gmail.com>
2025-09-15 18:25:28 -0400

    Add empty_list and dummy_list pytest fixtures to test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,51 @@
+from typing import List
 from app.main import hello_world
+from app.models.contact import Contact
+import uuid
+import pytest
 
 
-def test_create_client_returns_success():
+# TODO: Add test for valid phone number and email
+@pytest.fixture
+def empty_list():
+    return []
+
+
+@pytest.fixture
+def dummy_list():
+    return [
+        Contact(
+            contact_id=uuid.uuid4(),
+            first_name="John",
+            last_name="Doe",
+            email="dummy@example.com",
+        ),
+        Contact(
+            contact_id=uuid.uuid4(),
+            first_name="Jane",
+            last_name="Smith",
+            email="fake@notreal.com",
+            phone="1234567890",
+            company="Trucking Inc.",
+            industry="Shipping",
+            notes=["Contact early afternoons", "birthday in March"],
+        ),
+        Contact(
+            contact_id=uuid.uuid4(),
+            first_name="Adam",
+            last_name="West",
+            email="batman@justice.com",
+            phone=None,
+            company="Wayne Enterprises",
+        ),
+    ]
+
+
+def test_get_all_contacts(dummy_list: List[Contact]):
+    pass
+
+
+def test_create_client():
     pass
 
 
