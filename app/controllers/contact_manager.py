f89632e3812768db45b6d70617b6edaa22250a82
Hector <hector.keith91@gmail.com>
2025-09-15 19:19:47 -0400

    Move contact_manager.py to app/controllers

---
+++ app/controllers/contact_manager.py
@@ -0,0 +1,10 @@
+from app.models.contact import Contact
+import uuid
+
+
+class ContactManager:
+    def __init__(self):
+        contacts_list = []
+
+    def create_contact(self, new_contact: Contact, contacts_list):
+        pass
