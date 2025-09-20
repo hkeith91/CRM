1a0ea159b97916fc2ebfce21b63aa9d86c242ad3
Hector <hector.keith91@gmail.com>
2025-09-19 20:16:45 -0400

    Implement get_all_contacts

--- app/controllers/contact_manager.py
+++ app/controllers/contact_manager.py
@@ -7,3 +7,6 @@ class ContactManager:
 
     def create_contact(self, new_contact: Contact):
         self.contacts_list.append(new_contact)
+
+    def get_all_contacts(self):
+        return self.contacts_list
