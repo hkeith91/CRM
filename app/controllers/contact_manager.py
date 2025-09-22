ffeb3fb5ee24e8bb9db33591f0a63dce16eb4a5d
Hector <hector.keith91@gmail.com>
2025-09-22 02:39:31 -0400

    Add find_contact helper function

--- app/controllers/contact_manager.py
+++ app/controllers/contact_manager.py
@@ -1,14 +1,25 @@
 from app.models.contact import Contact
 
 
-# TODO: Add __eq__ to test equality
-# TODO: get contact by id
 # TODO: update contact by id
 # TODO: delete contact
+
+
 class ContactManager:
 
     def __init__(self):
         self.contacts_list = []
+
+    def find_contact(self, id_to_find: str):
+        found_contact = next(
+            (
+                contact
+                for contact in self.contacts_list
+                if contact.contact_id == id_to_find
+            ),
+            None,
+        )
+        return found_contact
 
     def create_contact(self, new_contact: Contact):
         """
@@ -27,12 +38,10 @@ class ContactManager:
         Finds a contact by their unique ID.
         Returns the Contact object if found, otherwise returns none.
         """
-        found_contact = next(
-            (
-                contact
-                for contact in self.contacts_list
-                if contact.contact_id == id_to_find
-            ),
-            None,
-        )
-        return found_contact
+        return self.find_contact(id_to_find)
+
+    def update_contact(self, contact_to_update_id: str):
+        found_contact = self.find_contact(contact_to_update_id)
+
+        if found_contact:
+            pass
